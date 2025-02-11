34f055a185552e4d18ed6d94abb519300c5b3020
Gabriel López <gla280900@gmail.com>
2025-02-11 10:12:41 -0600

    Update and rename lambda_function.py to function.py

---
+++ function.py
@@ -0,0 +1,10 @@
+import json
+
+print("TESTING CLONE FILE")
+
+#def lambda_handler(event, context):
+#    # TODO implement
+#    return {
+#        'statusCode': 200,
+#        'body': json.dumps('Hello from Lambda!')
+#    }
